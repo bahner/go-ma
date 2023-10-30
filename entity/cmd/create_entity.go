60de639a5572674421c7a35668bfe390a71d5183
Lars Bahner <lars.bahner@gmail.com>
2023-10-30 23:47:18 +0100

    Add command to create complete storable entity.

---
+++ entity/cmd/create_entity.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/bahner/go-ma/did"
+	"github.com/bahner/go-ma/entity"
+	"github.com/bahner/go-ma/key"
+)
+
+func main() {
+
+	fmt.Fprintln(os.Stderr, "******************************************************************")
+	fmt.Fprintln(os.Stderr, "*The following strings contains secrets and should not be shared.*")
+	fmt.Fprintln(os.Stderr, "*              It is only meant for testing.                     *")
+	fmt.Fprintln(os.Stderr, "******************************************************************")
+
+	var name string
+
+	flag.StringVar(&name, "name", "", "Name of the entity to create")
+	flag.Parse()
+
+	// Create a new person, object - an entity
+	ID, err := did.NewFromName(name)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new keyset for the entity
+	keyset, err := key.NewKeyset(ID)
+	if err != nil {
+		panic(err)
+	}
+
+	myEntity, err := entity.NewFromKeyset(keyset)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprint(os.Stderr, "Publishing Entity DIDDocument to IPFS. Please wait ...")
+	err = myEntity.PublishEntityDocument()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(os.Stderr, " done.")
+
+	document, err := myEntity.Doc.JSON()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(os.Stderr, "Published the following Entity DIDDocument: ")
+	fmt.Fprintln(os.Stderr, string(document))
+
+	packedEntity, err := myEntity.Pack()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(packedEntity)
+
+}
