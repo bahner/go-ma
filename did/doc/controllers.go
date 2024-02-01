1080d12e1c7633d95199d077d4ffbcd274d8b879
Lars Bahner <lars.bahner@gmail.com>
2024-02-01 01:20:41 +0100

    Add verification to controller addition.

--- did/doc/controllers.go
+++ did/doc/controllers.go
@@ -1,15 +1,32 @@
 package doc
 
-func (d *Document) AddController(controller string) {
+import (
+	"fmt"
+
+	"github.com/bahner/go-ma/did"
+)
+
+func (d *Document) AddController(controller string) error {
+
+	if d.Controller == nil {
+		return fmt.Errorf("controller slice is nil")
+	}
+
+	if !did.IsValidDID(controller) {
+		return fmt.Errorf("controller is not a valid DID: %s", controller)
+	}
+
 	// Check if the controller is already in the slice
 	for _, c := range d.Controller {
 		if c == controller {
-			return // Controller already exists, do nothing
+			return nil // Controller already exists, do nothing
 		}
 	}
 
 	// Append the new controller since it's not already present
 	d.Controller = append(d.Controller, controller)
+
+	return nil
 }
 func (d *Document) DeleteController(controller string) {
 	for i, c := range d.Controller {
